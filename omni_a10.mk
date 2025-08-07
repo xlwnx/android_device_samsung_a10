38706eba8d1f73da4e6d208dbd8dc8239f199a9b
xlwnx <105181270+xlwnx@users.noreply.github.com>
2025-08-07 20:20:17 +0700

    Update omni_a10.mk

--- omni_a10.mk
+++ omni_a10.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a10 device
 $(call inherit-product, device/samsung/a10/device.mk)
